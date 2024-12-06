49b8d8505938c3dddae6cffc3dbc93ff732d9bb6
Zr0-a1 <gmngf0rlfe@gmail.com>
2024-12-06 08:36:03 +0545

    first commit

---
+++ ArrayList.java
@@ -0,0 +1,21 @@
+
+public class ArrayList<T> {
+
+    public MessageListener get(int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+
+
+    }
+
+    public void add(Object messageListener) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    public void remove(Object messageListener) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+}
